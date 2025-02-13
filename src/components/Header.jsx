e3c030137760e76858239155ae843ab9b2f0b091
icancode333 <jyk637@utexas.edu>
2025-02-13 14:18:16 -0600

    edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import '../style.css';
 const Header = ({name}) => {
     return (
         <header className="header">
-            <h1 id="header-cust">Welcome {name} to the <span id="special">CodeCraft</span> Labs Intranet</h1>
+            <h1 id="header-cust">Welcome {name}, to the <span id="special">CodeCraft</span> Labs Intranet</h1>
             <nav>
                 <ul>
                     <li><a href="index.html">Home</a></li>
